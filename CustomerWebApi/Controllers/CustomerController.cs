d013ad89efa66039f51d5d24a7b3c187597d4244
MahmoudMardawi <ma.mardawi@gmail.com>
2022-12-11 08:49:22 +0200

    Create customer controller

---
+++ CustomerWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using CustomerWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext _customerDbContext;
+        public CustomerController(CustomerDbContext customerDbContext)
+        {
+            _customerDbContext= customerDbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            return _customerDbContext.Customers;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Customer>> GetById(int customerId)
+        {
+            var customer = await _customerDbContext.Customers.FindAsync(customerId);
+            return customer;
+        }
+
+        [HttpPost]
+           
+    }
+}
