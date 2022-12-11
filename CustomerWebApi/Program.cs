19e24cd13d3a38ae1e5e8f722abf1e2418c5d940
MahmoudMardawi <ma.mardawi@gmail.com>
2022-12-11 08:49:53 +0200

    Inject service builder in the program startup

--- CustomerWebApi/Program.cs
+++ CustomerWebApi/Program.cs
@@ -1,8 +1,18 @@
+using CustomerWebApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+var dbHost = "localhost";
+var dbName = "dms_customer";
+var dbPassword = "Ma@091847";
+var connectionstring = $"Data Source ={dbHost}; Initial Catalog ={dbName}; User ID = sa; Password ={dbPassword}";
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionstring));
 
 var app = builder.Build();
 
