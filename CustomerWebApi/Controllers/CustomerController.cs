3ec88610b9c3134af50ec11503c106df272e573b
MahmoudMardawi <ma.mardawi@gmail.com>
2022-12-11 14:22:58 +0200

    Complete API requests

--- CustomerWebApi/Controllers/CustomerController.cs
+++ CustomerWebApi/Controllers/CustomerController.cs
@@ -17,7 +17,7 @@ namespace CustomerWebApi.Controllers
         {
             return _customerDbContext.Customers;
         }
-        [HttpGet]
+        [HttpGet("{customerId:int}")]
         public async Task<ActionResult<Customer>> GetById(int customerId)
         {
             var customer = await _customerDbContext.Customers.FindAsync(customerId);
@@ -25,6 +25,28 @@ namespace CustomerWebApi.Controllers
         }
 
         [HttpPost]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            await _customerDbContext.Customers.AddAsync(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult> Update(Customer customer)
+        {
+            _customerDbContext.Customers.Update(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete("{customerId:int}")]
+        public async Task<ActionResult> Delete(int customerId)
+        {
+            var customer = await _customerDbContext.Customers.FindAsync(customerId);
+            _customerDbContext.Customers.Remove(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
            
     }
 }
